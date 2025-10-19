f22b880889b0ea8b95574c4e17ad447029fc2d35
Aryeh321 <Aryehzoust@gmail.com>
2025-10-18 23:47:41 -0400

    adds forbidden candies

--- list_and_function_excersizes.py
+++ list_and_function_excersizes.py
@@ -1,9 +1,11 @@
-def veggie_variety(vegetable, vegetable_N):
+def veggie_variety(vegetable, vegetable_N, forbidden_candy):
     
     formatted_list = ', '.join(vegetable)
-    return f"I make my kids eat {formatted_list} and even {vegetable_N}"
+    formatted_list_2 = ', '.join(forbidden_candy)
+    return f"I make my kids eat {formatted_list} and even {vegetable_N}, but would never let them eat {formatted_list_2}."
 
 veggie_list = ['carrot', 'broccoli', 'pepper', 'onion', 'tomato', 'mushroom']
 vegetable_N = 'spinach'
+forbidden_candy = ['lollies', 'taffies']
 
-print(veggie_variety(veggie_list[3:6], vegetable_N))
+print(veggie_variety(veggie_list[3:6], vegetable_N, forbidden_candy))
