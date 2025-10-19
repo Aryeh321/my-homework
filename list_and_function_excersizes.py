26194b5bff6e38b73c0ed20a7e2b5044f99af789
Aryeh321 <Aryehzoust@gmail.com>
2025-10-18 23:15:13 -0400

    eating all vegetables

---
+++ list_and_function_excersizes.py
@@ -0,0 +1,9 @@
+def veggie_variety(vegetable, vegetable_N):
+    
+    formatted_list = ', '.join(vegetable)
+    return f"I make my kids eat {formatted_list} and even {vegetable_N}"
+
+veggie_list = ['carrot', 'broccoli', 'pepper', 'onion', 'tomato', 'mushroom']
+vegetable_N = 'spinach'
+
+print(veggie_variety(veggie_list, vegetable_N))
