7c371905845cff34382cc1cec6432de31681aa64
Aryeh321 <Aryehzoust@gmail.com>
2025-10-19 20:31:33 -0400

    only adds the forbidden candy when they're in the list

--- eating_healthy.py
+++ eating_healthy.py
@@ -4,11 +4,14 @@ def eating_proper(vegetables, vegetable_N, forbidden_candy):
     formatted_list = ', '.join(vegetables)
     formatted_list_2 = ', '.join(forbidden_candy)
     
-    return f"I make my kids eat {formatted_list} and even {vegetable_N}, but would never let them eat {formatted_list_2}."
+    if forbidden_candy:
+        return f"I make my kids eat {formatted_list} and even {vegetable_N}, but would never let them eat {formatted_list_2}."
+    else:
+        return f"I make my kids eat {formatted_list} and even {vegetable_N}."
 
 with open('kids_foods.json', 'r') as f:
     data = json.load(f)
 
-output = eating_proper(data['vegetables'][6:11], data['vegetable_N'], data['forbidden_candy'])
+output = eating_proper(data['vegetables'][6:11], data['vegetable_N'], data.get('forbidden_candy', []))
 
 print(output)
