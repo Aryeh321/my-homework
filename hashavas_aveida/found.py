73a2097c81cf0f69c3d338db5f897441b86c4adc
Aryeh321 <Aryehzoust@gmail.com>
2025-10-22 22:24:12 -0400

    imports from Hashavas_aveida

--- hashavas_aveida/found.py
+++
@@ -1,6 +0,0 @@
-from classes import AveidahFinder
-
-finder = AveidahFinder()
-matches = finder.siman_checker(name="wallet", color="brown", size="small", shape="one fold", location="parking lot")
-for aveidah in matches:
-    print(f"Found: {aveidah.name}")
