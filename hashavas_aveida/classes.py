544b35da8480535c2eed039f37bf6afd1ad7d012
Aryeh321 <Aryehzoust@gmail.com>
2025-10-22 22:15:50 -0400

    code works now

--- hashavas_aveida/classes.py
+++ hashavas_aveida/classes.py
@@ -8,7 +8,16 @@ class Aveidah:
 
 class AveidahFinder:
     def __init__(self):
-        self.lost_objects = {Aveidah.name:"wallet", Aveidah.color:"brown", Aveidah.size:"small", Aveidah.shape:"one fold", Aveidah.location:"parking lot"}
+        self.lost_objects = [Aveidah(name="wallet", color="brown", size="small", shape="one fold", location="parking lot"),
+                             Aveidah(name="sippy cup", color="blue", size="small", shape="round", location="shul"),
+                             Aveidah(name="watch", color="silver", size="small", shape="round face", location="npgs"),
+                             Aveidah(name="pen", color="green", size="small", shape="parker", location="park"),
+]
 
     def siman_checker(self, name, color, size, shape, location):
-        return name == aveidah.name and color == aveidah.color and size == aveidah.size and shape == aveidah.shape and location == aveidah.location
+        found_objects = []
+        for obj in self.lost_objects:
+            if obj.name == name and obj.color == color and obj.size == size and obj.shape == shape and obj.location == location:
+                found_objects.append(obj)
+        
+        return found_objects
