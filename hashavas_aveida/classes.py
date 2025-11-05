7e20724c5077dda957e1b182ec420e760d47c67a
Aryeh321 <Aryehzoust@gmail.com>
2025-11-05 10:50:14 -0500

    extends class Aveidah to database, AveidahFinder gets objects from database

--- hashavas_aveida/classes.py
+++ hashavas_aveida/classes.py
@@ -1,18 +1,24 @@
-class Aveidah:
-    def __init__(self, name, color, size, shape, location):
-        self.name = name
-        self.color = color
-        self.size = size
-        self.shape = shape
-        self.location = location
+import sqlalchemy
+from sqlalchemy import create_engine, Column, DateTime, Integer, String
+from hashavas_aveida.database import Base, engine, Session
+
+class Aveidah(Base):
+    __tablename__ = 'aveidah'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    color = Column(String(100))
+    size = Column(String(100))
+    shape = Column(String(100))
+    location = Column(String(100))
+
+    def __repr__(self):
+        return f"<Aveidah(name='{self.name}', color='{self.color}', size='{self.size}', shape='{self.shape}', location='{self.location}')>"
+
 
 class AveidahFinder:
-    def __init__(self):
-        self.lost_objects = [Aveidah(name="wallet", color="brown", size="small", shape="one fold", location="parking lot"),
-                             Aveidah(name="sippy cup", color="blue", size="small", shape="round", location="shul"),
-                             Aveidah(name="watch", color="silver", size="small", shape="round face", location="npgs"),
-                             Aveidah(name="pen", color="green", size="small", shape="parker", location="park"),
-]
+    def __init__(self, db_session):
+        self.lost_objects = db_session.query(Aveidah).all()
 
     def siman_checker(self, name, color, size, shape, location):
         found_objects = []
